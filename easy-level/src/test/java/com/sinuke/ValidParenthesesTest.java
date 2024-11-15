a0d76a1477ae607a099408efe2289c0d3462a5cc
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-15 22:33:11 +0300

    added ValidParentheses

---
+++ easy-level/src/test/java/com/sinuke/ValidParenthesesTest.java
@@ -0,0 +1,20 @@
+package com.sinuke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ValidParenthesesTest {
+
+    @Test
+    void isValid() {
+        var validParentheses = new ValidParentheses();
+
+        assertTrue(validParentheses.isValid("()"));
+        assertTrue(validParentheses.isValid("()[]{}"));
+        assertFalse(validParentheses.isValid("(]"));
+        assertTrue(validParentheses.isValid("([])"));
+    }
+
+}
