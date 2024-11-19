02e7963fdd289be6013d42287a680c9537ab82b3
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-19 11:01:54 +0300

    added SearchInsertPosition

---
+++ easy-level/src/test/java/com/sinuke/SearchInsertPositionTest.java
@@ -0,0 +1,30 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchInsertPositionTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void searchInsert(int[] input, int target, int expected) {
+        var searchPosition = new SearchInsertPosition();
+
+        assertEquals(expected, searchPosition.searchInsert(input, target));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+            Arguments.of(new int[] {1, 3, 5, 6}, 5, 2),
+            Arguments.of(new int[] {1, 3, 5, 6}, 2, 1),
+            Arguments.of(new int[] {1, 3, 5, 6}, 7, 4),
+            Arguments.of(new int[] {1, 3, 5, 6}, 0, 0)
+        );
+    }
+
+}
