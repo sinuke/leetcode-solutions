fb82126746efe83064d3ce4e06e3978a550a7aef
Stanislau Ihnatovich <sinuke@tut.by>
2024-11-25 10:46:49 +0300

    add ReverseLinkedList

---
+++ easy-level/src/main/java/com/sinuke/ReverseLinkedList.java
@@ -0,0 +1,45 @@
+package com.sinuke;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        
+        var current = head;
+        while (current.next != null) {
+            var next = current.next;
+            current.next = next.next;
+            next.next = head;
+            head = next;
+        }
+        
+        return head;
+    }
+
+    public ListNode reverseListRecursive(ListNode head) {
+        if (head == null) return null;
+        return recursive(null, head);
+    }
+    
+    private ListNode recursive(ListNode previous, ListNode current) {
+        if (current.next == null) {
+            current.next = previous;
+            return current;
+        }
+        
+        var next = current.next;
+        current.next = previous;
+        
+        return recursive(current, next);
+    }
+    
+    public static class ListNode {
+        int val;
+        ListNode next;
+    
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    
+}
