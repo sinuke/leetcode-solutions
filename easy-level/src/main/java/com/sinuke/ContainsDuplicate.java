599a0f31002a9efa1a2375a90ac911b9d9a81611
Stanislau Ihnatovich <sinuke@tut.by>
2024-11-25 09:49:34 +0300

    added ContainsDuplicate

---
+++ easy-level/src/main/java/com/sinuke/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package com.sinuke;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+}
