a8da33ad7addee4f15ec8a2d46449f465cca028f
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-25 21:45:53 +0300

    init IntersectionTwoLinkedLists

---
+++ easy-level/src/main/java/com/sinuke/IntersectionTwoLinkedLists.java
@@ -0,0 +1,20 @@
+package com.sinuke;
+
+public class IntersectionTwoLinkedLists {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        return null;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/IntersectionTwoLinkedListsTest.java
@@ -0,0 +1,86 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class IntersectionTwoLinkedListsTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void getIntersectionNode(IntersectionTwoLinkedLists.ListNode headA, IntersectionTwoLinkedLists.ListNode headB, IntersectionTwoLinkedLists.ListNode intersection) {
+        // given
+        var intersectionTwoLinkedLists = new IntersectionTwoLinkedLists();
+        if (intersection != null) {
+            addIntersection(headA, intersection);
+            addIntersection(headB, intersection);
+        }
+
+        // when
+        var result = intersectionTwoLinkedLists.getIntersectionNode(headA, headB);
+        int[] intersectionArray = extractValues(intersection);
+        int[] resultArray = extractValues(result);
+
+        // then
+        assertArrayEquals(intersectionArray, resultArray);
+    }
+    
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(createList(4, 1), createList(5, 6, 1), createList(8, 4, 5)),
+                Arguments.of(createList(1, 9, 1), createList(3), createList(2, 4)),
+                Arguments.of(createList(2, 6, 4), createList(1, 5), createList())
+        );
+    }
+
+    private static IntersectionTwoLinkedLists.ListNode createList(int... values) {
+        if (values.length == 0) return null;
+
+        var head = new IntersectionTwoLinkedLists.ListNode(values[0]);
+        var previous = head;
+        for (int i = 1; i < values.length; i++) {
+            var element = new IntersectionTwoLinkedLists.ListNode(values[i]);
+            previous.next = element;
+            previous = element;
+        }
+
+        return head;
+    }
+
+    private static int[] extractValues(IntersectionTwoLinkedLists.ListNode node) {
+
+        int count = 0;
+        var first = node;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+
+        var result = new int[count];
+        node = first;
+        var index = 0;
+        while (node != null) {
+            result[index] = node.val;
+            node = node.next;
+            index++;
+        }
+
+        return result;
+    }
+
+    private static void addIntersection(IntersectionTwoLinkedLists.ListNode list, IntersectionTwoLinkedLists.ListNode intersection) {
+        var current = list;
+        while (current != null) {
+            if (current.next == null) {
+                current.next = intersection;
+                break;
+            }
+            current = current.next;
+        }
+    }
+    
+}
