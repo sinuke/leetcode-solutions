6fac9e7adb0bafb7f4831796335fe854b07c0383
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-26 23:29:59 +0300

    add DuplicateEmails

--- easy-level/src/main/sql/DuplicateEmails.sql
+++ easy-level/src/main/sql/DuplicateEmails.sql
@@ -2,4 +2,10 @@ SELECT email
 FROM (SELECT email, COUNT(id) AS cnt
       FROM Person
       GROUP BY email) aggr
-WHERE cnt > 1
+WHERE cnt > 1;
+
+-- from LeetCode solutions
+-- SELECT email
+-- FROM Person
+-- GROUP BY email
+-- HAVING COUNT(email) > 1;
