2fecc6af9c461286992bc64c1ff6c2c36a841ddf
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-27 23:08:17 +0300

    add RisingTemperature.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/RisingTemperatureTest.java
@@ -0,0 +1,29 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class RisingTemperatureTest extends SqlTestBase {
+
+    private final Map<String, List<Integer>> expected = Map.of(
+            "id", List.of(2, 4)
+    );
+
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/RisingTemperature/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/RisingTemperature/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/RisingTemperature.sql", expected, 2)
+        );
+    }
+
+}
---
+++ easy-level/src/test/resources/RisingTemperature/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS Weather;
+CREATE TABLE Weather (
+    id INT PRIMARY KEY,
+    recordDate DATE,
+    temperature INT
+);
