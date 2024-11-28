c0eeb24f687d33421f97b39fd48c4dfada95d5ef
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-28 15:08:59 +0300

    add ClassesMoreThanFiveStudents.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/ClassesMoreThanFiveStudentsTest.java
@@ -0,0 +1,28 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ClassesMoreThanFiveStudentsTest extends SqlTestBase {
+
+    private final Map<String, List<String>> expected = Map.of(
+            "class", List.of("Math")
+    );
+
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/ClassesMoreThanFiveStudents/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/ClassesMoreThanFiveStudents/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/ClassesMoreThanFiveStudents.sql", expected, 1)
+        );
+    }
+}
