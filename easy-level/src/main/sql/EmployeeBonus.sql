7e650dc6c2ea7bd3859f0564be1b45eb47d77b82
Stanislau Ihnatovich <sinuke@tut.by>
2024-11-28 11:06:37 +0300

    add EmployeeBonus.sql

---
+++ easy-level/src/main/sql/EmployeeBonus.sql
@@ -0,0 +1,5 @@
+SELECT e.name, bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE bonus IS NULL OR bonus < 1000;
---
+++ easy-level/src/test/java/com/sinuke/sql/EmployeeBonusTest.java
@@ -0,0 +1,31 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class EmployeeBonusTest extends SqlTestBase {
+    
+    private final Map<String, List<Object>> expected = Map.of(
+            "name", List.of("Brad", "John", "Dan"),
+            "bonus", Arrays.asList(null, null, 500)
+    );
+    
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/EmployeeBonus/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/EmployeeBonus/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/EmployeeBonus.sql", expected, 3)
+        );
+    }
+    
+}
