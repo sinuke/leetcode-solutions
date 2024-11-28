d58802ea3593b61a44ebcde682d2117c11211a21
Stanislau Ihnatovich <sinuke@tut.by>
2024-11-28 11:30:12 +0300

    add FindCustomerReferee.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/FindCustomerRefereeTest.java
@@ -0,0 +1,29 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class FindCustomerRefereeTest extends SqlTestBase {
+    
+    private final Map<String, List<String>> expected = Map.of(
+            "name", List.of("Will", "Jane", "Bill", "Zack")
+    );
+    
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/FindCustomerReferee/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/FindCustomerReferee/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/FindCustomerReferee.sql", expected, 4)
+        );
+    }
+    
+}
