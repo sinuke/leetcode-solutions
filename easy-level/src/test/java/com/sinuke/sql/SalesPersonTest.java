b3863799d32e77c13319ea8fd99c19bb012adeb9
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-28 16:09:48 +0300

    add SalesPerson.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/SalesPersonTest.java
@@ -0,0 +1,29 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class SalesPersonTest extends SqlTestBase {
+
+    private final Map<String, List<String>> expected = Map.of(
+            "name", List.of("Amy", "Mark", "Alex")
+    );
+
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/SalesPerson/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/SalesPerson/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/SalesPerson.sql", expected, 3)
+        );
+    }
+
+}
