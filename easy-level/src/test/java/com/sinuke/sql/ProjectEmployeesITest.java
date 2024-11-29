4f3eb359afabf813fc73393c4814b93dd2920e63
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-11-29 23:40:56 +0300

    add ProjectEmployeesI.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/ProjectEmployeesITest.java
@@ -0,0 +1,30 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class ProjectEmployeesITest extends SqlTestBase {
+
+    private final Map<String, List<Object>> expected = Map.of(
+            "project_id", List.of(1, 2),
+            "average_years", List.of(2.00f, 2.50f)
+    );
+
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/ProjectEmployeesI/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/ProjectEmployeesI/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/ProjectEmployeesI.sql", expected, 2)
+        );
+    }
+
+}
