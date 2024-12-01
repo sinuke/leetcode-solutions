34e0f5f460d5a0370b4ac32323e232f330467257
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-01 22:41:05 +0300

    add TopTravellers.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/TopTravellersTest.java
@@ -0,0 +1,30 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class TopTravellersTest extends SqlTestBase {
+
+    private final Map<String, List<Object>> expected = Map.of(
+            "name", List.of("Elvis", "Lee", "Bob", "Jonathan", "Alex", "Alice"),
+            "travelled_distance", List.of(450, 450, 317, 312, 222, 120)
+    );
+
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/TopTravellers/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/TopTravellers/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/TopTravellers.sql", expected, 6)
+        );
+    }
+
+}
