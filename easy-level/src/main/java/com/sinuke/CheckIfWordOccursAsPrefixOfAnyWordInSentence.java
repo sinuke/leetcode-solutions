f0aa37b051a006a9bfdd67b355416d08c2367636
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-02 22:47:39 +0300

    add CheckIfWordOccursAsPrefixOfAnyWordInSentence

---
+++ easy-level/src/main/java/com/sinuke/CheckIfWordOccursAsPrefixOfAnyWordInSentence.java
@@ -0,0 +1,15 @@
+package com.sinuke;
+
+public class CheckIfWordOccursAsPrefixOfAnyWordInSentence {
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        var words = sentence.split(" ");
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].startsWith(searchWord)) return i + 1;
+        }
+
+        return -1;
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/CheckIfWordOccursAsPrefixOfAnyWordInSentenceTest.java
@@ -0,0 +1,28 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CheckIfWordOccursAsPrefixOfAnyWordInSentenceTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void isPrefixOfWord(String sentence, String searchWord, int expected) {
+        var checkOfWord = new CheckIfWordOccursAsPrefixOfAnyWordInSentence();
+        assertEquals(expected, checkOfWord.isPrefixOfWord(sentence, searchWord));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("i love eating burger", "burg", 4),
+                Arguments.of("this problem is an easy problem", "pro", 2),
+                Arguments.of("i am tired", "you", -1)
+        );
+    }
+
+}
