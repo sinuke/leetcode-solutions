88af71be54bfb5597bf21c776e6b6f88500f0337
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-03 22:10:31 +0300

    add DailyLeadsAndPartners.sql

---
+++ easy-level/src/test/java/com/sinuke/sql/DailyLeadsAndPartnersTest.java
@@ -0,0 +1,32 @@
+package com.sinuke.sql;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public class DailyLeadsAndPartnersTest extends SqlTestBase {
+
+    private final Map<String, List<Object>> expected = Map.of(
+            "date_id", List.of("2020-12-07", "2020-12-07", "2020-12-08", "2020-12-08"),
+            "make_name", List.of("honda", "toyota", "honda", "toyota"),
+            "unique_leads", List.of(3, 1, 2, 2),
+            "unique_partners", List.of(2, 2, 2, 3)
+    );
+
+    @Override
+    public void setup(Statement statement) throws Exception {
+        statement.execute("RUNSCRIPT FROM 'classpath:/DailyLeadsAndPartners/schema.sql'");
+        statement.execute("RUNSCRIPT FROM 'classpath:/DailyLeadsAndPartners/data.sql'");
+    }
+
+    @Override
+    protected Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("src/main/sql/DailyLeadsAndPartners.sql", expected, 4)
+        );
+    }
+
+}
---
+++ easy-level/src/test/resources/DailyLeadsAndPartners/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-8', 'toyota', '0', '1');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-8', 'toyota', '1', '0');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-8', 'toyota', '1', '2');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-7', 'toyota', '0', '2');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-7', 'toyota', '0', '1');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-8', 'honda', '1', '2');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-8', 'honda', '2', '1');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-7', 'honda', '0', '1');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-7', 'honda', '1', '2');
+INSERT INTO DailySales (date_id, make_name, lead_id, partner_id) VALUES ('2020-12-7', 'honda', '2', '1');
