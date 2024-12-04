efec04eee5110aad431030304968420ad8af4a7a
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-04 22:49:35 +0300

    add RearrangeProductsTable.sql

---
+++ easy-level/src/test/resources/RearrangeProductsTable/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Products (product_id, store1, store2, store3) VALUES ('0', '95', '100', '105');
+INSERT INTO Products (product_id, store1, store2, store3) VALUES ('1', '70', NULL, '80');
---
+++ easy-level/src/test/resources/RearrangeProductsTable/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS Products;
+CREATE TABLE Products (
+    product_id INT,
+    store1 INT,
+    store2 INT,
+    store3 INT
+);
