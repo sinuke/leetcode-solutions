e3585d72fa1b3b77039f6cd19aae8baf0395f96b
Stanislau Ihnatovich <sinuke@tut.by>
2024-12-04 16:36:25 +0300

    add FindFollowersCount.sql

---
+++ easy-level/src/test/resources/FindFollowersCount/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Followers (user_id, follower_id) VALUES ('0', '1');
+INSERT INTO Followers (user_id, follower_id) VALUES ('1', '0');
+INSERT INTO Followers (user_id, follower_id) VALUES ('2', '0');
+INSERT INTO Followers (user_id, follower_id) VALUES ('2', '1');
