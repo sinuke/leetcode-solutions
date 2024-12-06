95b1eb3ff153a341d36f4de5f7c9432c6bb6b678
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-06 22:30:06 +0300

    add "Number of 1 Bits"

---
+++ easy-level/src/test/java/com/sinuke/NumberOf1BitsTest.java
@@ -0,0 +1,28 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NumberOf1BitsTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void hammingWeight(int n, int expected) {
+        var numberOf1Bits = new NumberOf1Bits();
+        assertEquals(expected, numberOf1Bits.hammingWeight(n));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(11, 3),
+                Arguments.of(128, 1),
+                Arguments.of(2147483645, 30)
+        );
+    }
+
+}
