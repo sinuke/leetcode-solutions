6ff7693070defc5fe6212d890553cdb7977c6d6c
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-06 08:55:50 +0300

    add MaximumNumberOfIntegersToChooseFromRangeI

---
+++ medium-level/src/main/java/com/sinuke/MaximumNumberOfIntegersToChooseFromRangeI.java
@@ -0,0 +1,26 @@
+package com.sinuke;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class MaximumNumberOfIntegersToChooseFromRangeI {
+
+    public int maxCount(int[] banned, int n, int maxSum) {
+        Set<Integer> bannedSet = Arrays.stream(banned).boxed().collect(Collectors.toSet());
+
+        int k = 0;
+        int i = 1;
+        int sum = 0;
+
+        while (i <= n && sum <= maxSum) {
+            if (!bannedSet.contains(i)) {
+                sum += i;
+                if (sum <= maxSum) k++;
+            }
+            i++;
+        }
+        return k;
+    }
+
+}
---
+++ medium-level/src/test/java/com/sinuke/MaximumNumberOfIntegersToChooseFromRangeITest.java
@@ -0,0 +1,30 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class MaximumNumberOfIntegersToChooseFromRangeITest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void maxCount(int[] banned, int n, int maxSum, int expected) {
+        var maximumNumbers = new MaximumNumberOfIntegersToChooseFromRangeI();
+        assertEquals(expected, maximumNumbers.maxCount(banned, n, maxSum));
+    }
+
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(new int[] {1, 5, 6}, 5, 6, 2),
+                Arguments.of(new int[] {1, 2, 3, 4, 5, 6, 7}, 8, 1, 0),
+                Arguments.of(new int[] {11}, 7, 50, 7)
+        );
+    }
+
+}
