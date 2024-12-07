0885cee793dd17c7ae573e32ee6a29a8b9089fae
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-07 22:33:41 +0300

    add "Rank Scores"

---
+++ medium-level/sql/178. Rank Scores/test/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO Scores (id, score) VALUES ('1', '3.5');
+INSERT INTO Scores (id, score) VALUES ('2', '3.65');
+INSERT INTO Scores (id, score) VALUES ('3', '4.0');
+INSERT INTO Scores (id, score) VALUES ('4', '3.85');
+INSERT INTO Scores (id, score) VALUES ('5', '4.0');
+INSERT INTO Scores (id, score) VALUES ('6', '3.65');
---
+++ medium-level/sql/178. Rank Scores/test/schema.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS Scores;
+CREATE TABLE Scores (
+    id INT,
+    score DECIMAL(3, 2)
+);
