f8ab7c7ca62e7bfc6c64313606ddccd49f1f4885
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-07 19:44:49 +0300

    initial migration to test containers from H2

--- common/src/main/java/com/sinuke/common/SQLSolutionsTest.java
+++ common/src/main/java/com/sinuke/common/SQLSolutionsTest.java
@@ -7,25 +7,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
-import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -33,30 +38,46 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+@Testcontainers
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class SQLSolutionsTest {
 
-    protected static Map<String, TestData> sqlFilesWithTests;
-    protected static Connection connection;
+    private static final String MYSQL_CONTAINER_WITH_VERSION = "mysql:8.0.26";
+    private static final String TEST_DB_NAME = "test_db";
+    private static final String TEST_USER = "test_user";
+    private static final String TEST_PASSWORD = "test_password";
+
+    private MySQLContainer<?> mysqlContainer;
+    private Connection connection;
+
+    protected Map<String, TestData> sqlFilesWithTests;
 
     @BeforeAll
-    protected final void basicSetup() throws Exception {
-        var ds = new JdbcDataSource();
-        ds.setURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;MODE=MySQL");
-        connection = ds.getConnection();
+    protected final void setUp() throws Exception {
+        mysqlContainer = new MySQLContainer<>(MYSQL_CONTAINER_WITH_VERSION)
+                .withDatabaseName(TEST_DB_NAME)
+                .withUsername(TEST_USER)
+                .withPassword(TEST_PASSWORD);
+        mysqlContainer.start();
+
+        var properties = new Properties();
+        properties.setProperty("user", TEST_USER);
+        properties.setProperty("password", TEST_PASSWORD);
+
+        connection = mysqlContainer.getJdbcDriverInstance().connect(mysqlContainer.getJdbcUrl(), properties);
 
         sqlFilesWithTests = scanDirectory(Paths.get("sql/"));
     }
 
     @AfterAll
-    protected void teardown() throws Exception {
+    protected final void tearDown() throws Exception {
         if (connection != null) connection.close();
+        if (mysqlContainer != null) mysqlContainer.close();
     }
 
     @ParameterizedTest(name = "{index}: {0}")
     @MethodSource("testData")
-    @SneakyThrows
-    void sqlSolutionTest(TestData testData, String sqlFile) {
+    void sqlSolutionTest(TestData testData, String sqlFile) throws Exception {
         assertNotNull(testData);
         assumeTrue(testData.isEnabled());
 
@@ -65,10 +86,10 @@ public abstract class SQLSolutionsTest {
         var sql = Files.readString(sqlFilePath);
         try (var statement = connection.createStatement()) {
             // given
-            var schemaPath = sqlFilePath.getParent().resolve("test/" + testData.getSchema()).toString();
-            statement.execute(String.format("RUNSCRIPT FROM '%s'", schemaPath));
-            var dataPath = sqlFilePath.getParent().resolve("test/" + testData.getData()).toString();
-            statement.execute(String.format("RUNSCRIPT FROM '%s'", dataPath));
+            var schema = Files.readString(sqlFilePath.getParent().resolve("test/" + testData.getSchema()), StandardCharsets.UTF_8);
+            executeSQLContent(statement, schema);
+            var data = Files.readString(sqlFilePath.getParent().resolve("test/" + testData.getData()), StandardCharsets.UTF_8);
+            executeSQLContent(statement, data);
 
             // when
             var hasResultSet = statement.execute(sql);
@@ -89,7 +110,7 @@ public abstract class SQLSolutionsTest {
         }
     }
 
-    private static void assertValue(ResultSet resultSet, int i, Map.Entry<String, List<Object>> entry) throws SQLException {
+    private void assertValue(ResultSet resultSet, int i, Map.Entry<String, List<Object>> entry) throws SQLException {
         Object value;
 
         switch (entry.getValue().get(i)) {
@@ -105,15 +126,14 @@ public abstract class SQLSolutionsTest {
         assertEquals(entry.getValue().get(i), value);
     }
 
-    private static Stream<Arguments> testData() {
+    private Stream<Arguments> testData() {
         return sqlFilesWithTests.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue(Comparator.comparing(TestData::getNumber)))
                 .map(entry -> Arguments.of(entry.getValue(), entry.getKey()));
     }
 
-    @SneakyThrows
-    private static Map<String, TestData> scanDirectory(Path rootDir) {
+    private Map<String, TestData> scanDirectory(Path rootDir) throws IOException {
         Map<String, TestData> result = new HashMap<>();
         var mapper = new ObjectMapper();
 
@@ -132,8 +152,14 @@ public abstract class SQLSolutionsTest {
         return result;
     }
 
+    private void executeSQLContent(Statement statement, String content) throws Exception {
+        for (var query : content.split(";")) {
+            if (!query.trim().isEmpty()) statement.execute(query);
+        }
+    }
+
     @SneakyThrows
-    private static TestData parseTestDataFromFile(ObjectMapper mapper, File testDataFile) {
+    private TestData parseTestDataFromFile(ObjectMapper mapper, File testDataFile) {
         return mapper.readValue(testDataFile, TestData.class);
     }
 
