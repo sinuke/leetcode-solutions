2b479327d13cf876d2fd1984e763b4d5027844e0
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-08 06:55:23 +0300

    add "Add Two Numbers"

---
+++ medium-level/src/main/java/com/sinuke/AddTwoNumbers.java
@@ -0,0 +1,64 @@
+package com.sinuke;
+
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int additional = 0;
+
+        var curL1 = l1;
+        var curL2 = l2;
+
+        var result = new ListNode();
+        var curResult = result;
+
+        while (curL1 != null || curL2 != null) {
+            int sum = 0;
+
+            if (curL1 != null && curL2 != null) {
+                sum = curL1.val + curL2.val + additional;
+                if (sum >= 10) {
+                    additional = 1;
+                    sum -= 10;
+                } else additional = 0;
+                curL1 = curL1.next;
+                curL2 = curL2.next;
+            } else if (curL1 != null) {
+                sum = curL1.val + additional;
+                if (sum >= 10) {
+                    additional = 1;
+                    sum -= 10;
+                }
+                else additional = 0;
+                curL1 = curL1.next;
+            } else {
+                sum = curL2.val + additional;
+                if (sum >= 10) {
+                    additional = 1;
+                    sum -= 10;
+                }
+                else additional = 0;
+                curL2 = curL2.next;
+            }
+
+            curResult.val = sum;
+            if (curL1 != null || curL2 != null || additional == 1) {
+                curResult.next = new ListNode();
+                curResult = curResult.next;
+            }
+        }
+
+        if (additional == 1) curResult.val = additional;
+
+        return result;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode() {}
+        public ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+}
---
+++ medium-level/src/test/java/com/sinuke/AddTwoNumbersTest.java
@@ -0,0 +1,70 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class AddTwoNumbersTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void addTwoNumbers(AddTwoNumbers.ListNode l1, AddTwoNumbers.ListNode l2, AddTwoNumbers.ListNode expected) {
+        var addTwoNumbers = new AddTwoNumbers();
+
+        var result = addTwoNumbers.addTwoNumbers(l1, l2);
+
+        var expectedArray = extractValues(expected);
+        var actualArray = extractValues(result);
+
+        assertArrayEquals(expectedArray, actualArray);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(createList(2, 4, 3), createList(5, 6, 4), createList(7, 0, 8)),
+                Arguments.of(createList(0), createList(0), createList(0)),
+                Arguments.of(createList(9, 9, 9, 9, 9, 9, 9), createList(9, 9, 9, 9), createList(8, 9, 9, 9, 0, 0, 0, 1)),
+                Arguments.of(createList(1, 8), createList(0), createList(1, 8))
+        );
+    }
+
+    private static AddTwoNumbers.ListNode createList(int... values) {
+        if (values.length == 0) return null;
+
+        var head = new AddTwoNumbers.ListNode(values[0]);
+        var previous = head;
+        for (int i = 1; i < values.length; i++) {
+            var element = new AddTwoNumbers.ListNode(values[i]);
+            previous.next = element;
+            previous = element;
+        }
+
+        return head;
+    }
+
+    private static int[] extractValues(AddTwoNumbers.ListNode node) {
+
+        int count = 0;
+        var first = node;
+        while (node != null) {
+            count++;
+            node = node.next;
+        }
+
+        var result = new int[count];
+        node = first;
+        var index = 0;
+        while (node != null) {
+            result[index] = node.val;
+            node = node.next;
+            index++;
+        }
+
+        return result;
+    }
+
+}
