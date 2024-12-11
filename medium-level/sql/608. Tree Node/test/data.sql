a9ab7238233c414d85dfc432d90af79cd91505e3
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-11 19:19:39 +0300

    add "Tree Node"

---
+++ medium-level/sql/608. Tree Node/test/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Tree (id, p_id) VALUES ('1', NULL);
+INSERT INTO Tree (id, p_id) VALUES ('2', '1');
+INSERT INTO Tree (id, p_id) VALUES ('3', '1');
+INSERT INTO Tree (id, p_id) VALUES ('4', '2');
+INSERT INTO Tree (id, p_id) VALUES ('5', '2');
---
+++ medium-level/sql/608. Tree Node/test/schema.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS Tree;
+CREATE TABLE Tree (
+    id INT,
+    p_id INT
+);
