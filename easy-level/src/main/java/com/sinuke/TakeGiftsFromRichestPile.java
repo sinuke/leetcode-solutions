e25d6d283581a76b4d6a41e82a901e21badbae57
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-13 00:12:13 +0300

    add "Take Gifts From the Richest Pile"

---
+++ easy-level/src/main/java/com/sinuke/TakeGiftsFromRichestPile.java
@@ -0,0 +1,31 @@
+package com.sinuke;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class TakeGiftsFromRichestPile {
+
+    public long pickGifts(int[] gifts, int k) {
+        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        long sum = 0;
+        for (int i : gifts) {
+            pq.add(i);
+            sum += i;
+        }
+
+        int i = 0;
+        while (i < k) {
+            int value = pq.poll();
+            sum -= value;
+            int sqrt = (int) Math.sqrt(value);
+            sum += sqrt;
+
+            pq.add(sqrt);
+            i++;
+        }
+
+        return sum;
+    }
+
+}
