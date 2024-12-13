e11310d452c9eafedbc073cb0f5af62f88a4e6ac
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-13 08:38:21 +0300

    add "Product Price at a Given Date"

--- medium-level/sql/1164. Product Price at a Given Date/Product Price at a Given Date.sql
+++ medium-level/sql/1164. Product Price at a Given Date/Product Price at a Given Date.sql
@@ -15,24 +15,13 @@ t3 AS (
     ON t1.product_id = t2.product_id AND t1.change_date = t2.change_date
 ),
 t4 AS (
-    SELECT product_id, new_price, change_date
+    SELECT DISTINCT product_id, 10 AS price
     FROM Products
-    WHERE change_date > '2019-08-16' AND product_id NOT IN (SELECT product_id FROM t1)
-),
-t5 AS (
-    SELECT product_id, MIN(change_date) AS change_date
-    FROM t4
-    GROUP BY product_id
-),
-t6 AS (
-    SELECT t4.product_id, 10 AS price
-    FROM t4
-    JOIN t5
-    ON t4.product_id = t5.product_id AND t4.change_date = t5.change_date
+    WHERE product_id NOT IN (SELECT product_id FROM t1)
 )
 SELECT *
 FROM t3
 UNION ALL
 SELECT *
-FROM t6
+FROM t4
 ORDER BY product_id;
