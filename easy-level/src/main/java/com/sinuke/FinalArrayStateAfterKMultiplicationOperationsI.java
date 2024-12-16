60ca3589ec8064df7365663d812bcc9ff1f1d4f4
Stanislau Ihnatovich <sinuke@tut.by>
2024-12-16 11:53:37 +0300

    refactor "Final Array State After K Multiplication Operations I"

--- easy-level/src/main/java/com/sinuke/FinalArrayStateAfterKMultiplicationOperationsI.java
+++ easy-level/src/main/java/com/sinuke/FinalArrayStateAfterKMultiplicationOperationsI.java
@@ -1,6 +1,5 @@
 package com.sinuke;
 
-import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
@@ -9,7 +8,7 @@ public class FinalArrayStateAfterKMultiplicationOperationsI {
     public int[] getFinalState(int[] nums, int k, int multiplier) {
         if (multiplier == 1) return nums;
 
-        Queue<int[]> pq = new PriorityQueue<>(nums.length, Comparator.comparing((int[] a) -> a[0]).thenComparing(a -> a[1]));
+        Queue<int[]> pq = new PriorityQueue<>(nums.length, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
 
         for (int i = 0; i < nums.length; i++) {
             pq.add(new int[] {nums[i], i});
