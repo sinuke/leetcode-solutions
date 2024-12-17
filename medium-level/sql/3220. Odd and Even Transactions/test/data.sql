9fc2076cab11f745d8ae9eb63520c52a1b1d97cf
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-17 06:53:46 +0300

    add "Odd and Even Transactions"

---
+++ medium-level/sql/3220. Odd and Even Transactions/test/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO transactions (transaction_id, amount, transaction_date) VALUES ('1', '150', '2024-07-01');
+INSERT INTO transactions (transaction_id, amount, transaction_date) VALUES ('2', '200', '2024-07-01');
+INSERT INTO transactions (transaction_id, amount, transaction_date) VALUES ('3', '75', '2024-07-01');
+INSERT INTO transactions (transaction_id, amount, transaction_date) VALUES ('4', '300', '2024-07-02');
+INSERT INTO transactions (transaction_id, amount, transaction_date) VALUES ('5', '50', '2024-07-02');
+INSERT INTO transactions (transaction_id, amount, transaction_date) VALUES ('6', '120', '2024-07-03');
---
+++ medium-level/sql/3220. Odd and Even Transactions/test/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS transactions;
+CREATE TABLE transactions (
+    transaction_id INT,
+    amount INT,
+    transaction_date DATE
+);
