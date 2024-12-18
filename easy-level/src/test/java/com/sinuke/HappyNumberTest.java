d0961f292bf12a6822daf72dcd6fa6f6e4f59b48
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-18 06:51:02 +0300

    add "Happy Number"

---
+++ easy-level/src/test/java/com/sinuke/HappyNumberTest.java
@@ -0,0 +1,29 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HappyNumberTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void isHappy(int n, boolean expected) {
+        var happy = new HappyNumber();
+        assertEquals(expected, happy.isHappy(n));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(19, true),
+                Arguments.of(2, false),
+                Arguments.of(4, false),
+                Arguments.of(1111111, true)
+        );
+    }
+
+}
