ffada633c3cfa887243992b47d3aa59c298a351a
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-21 23:51:46 +0300

    optimize "First Bad Version"

--- easy-level/src/main/java/com/sinuke/FirstBadVersion.java
+++ easy-level/src/main/java/com/sinuke/FirstBadVersion.java
@@ -15,9 +15,9 @@ public class FirstBadVersion {
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
-        while (left <= right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) right = mid - 1;
+            if (isBadVersion(mid)) right = mid;
             else left = mid + 1;
         }
         return left;
