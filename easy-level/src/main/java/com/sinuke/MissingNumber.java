eee2c723e88a0b39bc53424e04ce06a06cdf166f
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-21 17:46:38 +0300

    add "Missing Number"

---
+++ easy-level/src/main/java/com/sinuke/MissingNumber.java
@@ -0,0 +1,29 @@
+package com.sinuke;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            if (i != nums[i]) return i;
+        }
+
+        return n;
+    }
+
+    public int missingNumber2(int[] nums) {
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+
+        for (int num : nums) {
+            sum -= num;
+        }
+
+        return sum;
+    }
+
+}
