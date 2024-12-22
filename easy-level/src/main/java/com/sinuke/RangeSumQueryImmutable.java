15c7f2fea3cce62b0c85143b15adf6f595e5692d
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-22 23:50:05 +0300

    add "Range Sum Query - Immutable"

---
+++ easy-level/src/main/java/com/sinuke/RangeSumQueryImmutable.java
@@ -0,0 +1,25 @@
+package com.sinuke;
+
+public class RangeSumQueryImmutable {
+
+    public static class NumArray {
+
+        private final int[] sums;
+
+        public NumArray(int[] nums) {
+            this.sums = new int[nums.length + 1];
+            if (nums.length == 0) return;
+
+            sums[0] = nums[0];
+            for (int i = 0; i < nums.length; i++) {
+                sums[i + 1] = sums[i] + nums[i];
+            }
+        }
+
+        public int sumRange(int left, int right) {
+            return sums[right + 1] - sums[left];
+        }
+
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/RangeSumQueryImmutableTest.java
@@ -0,0 +1,49 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class RangeSumQueryImmutableTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void rangeSumQueryImmutableTest(String[] operations, int[][] values, Object[] expected) {
+        RangeSumQueryImmutable.NumArray numArray = null;
+        Object[] actual = new Object[operations.length];
+
+        for (int i = 0; i < operations.length; i++) {
+            switch (operations[i]) {
+                case "NumArray": {
+                    numArray = new RangeSumQueryImmutable.NumArray(values[i]);
+                    actual[i] = null;
+                    break;
+                }
+
+                case "sumRange": {
+                    actual[i] = numArray.sumRange(values[i][0], values[i][1]);
+                    break;
+                }
+
+                default: throw new IllegalArgumentException("Unknown operation: " + operations[i]);
+            }
+        }
+
+        assertArrayEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(
+                        new String[] {"NumArray", "sumRange", "sumRange", "sumRange"},
+                        new int[][] {{-2, 0, 3, -5, 2, -1}, {0, 2}, {2, 5}, {0, 5}},
+                        new Object[] {null, 1, -1, -3}
+                )
+        );
+    }
+
+}
