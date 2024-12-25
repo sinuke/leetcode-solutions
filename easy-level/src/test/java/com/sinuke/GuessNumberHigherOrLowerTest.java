3ef2f8bed15dfd53e194643d249ad816e17c01ae
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-25 18:58:24 +0300

    add "Guess Number Higher or Lower"

---
+++ easy-level/src/test/java/com/sinuke/GuessNumberHigherOrLowerTest.java
@@ -0,0 +1,28 @@
+package com.sinuke;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GuessNumberHigherOrLowerTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void guessNumberHigherOrLower(int n, int pick) {
+        var guessNumber = new GuessNumberHigherOrLower(pick);
+        assertEquals(pick, guessNumber.guessNumber(n));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(10, 6),
+                Arguments.of(1, 1),
+                Arguments.of(2, 1)
+        );
+    }
+
+}
