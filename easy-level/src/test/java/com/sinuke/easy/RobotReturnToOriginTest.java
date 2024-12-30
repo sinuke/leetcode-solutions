bf8bba0eda673dca9664d6f8c19326442c07ca78
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2024-12-30 23:32:01 +0300

    add "Robot Return to Origin"

---
+++ easy-level/src/test/java/com/sinuke/easy/RobotReturnToOriginTest.java
@@ -0,0 +1,28 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RobotReturnToOriginTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void judgeCircle(String moves, boolean expected) {
+        var robot = new RobotReturnToOrigin();
+        assertEquals(expected, robot.judgeCircle(moves));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("UD", true),
+                Arguments.of("RDLLUURD", true),
+                Arguments.of("LL", false)
+        );
+    }
+
+}
