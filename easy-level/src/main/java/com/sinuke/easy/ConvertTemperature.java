954c374a45bec32071aad48aaef4bb77283a5ebf
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-01-02 16:10:35 +0300

    add "Convert the Temperature"

--- easy-level/src/main/java/com/sinuke/easy/ConvertTemperature.java
+++ easy-level/src/main/java/com/sinuke/easy/ConvertTemperature.java
@@ -3,10 +3,7 @@ package com.sinuke.easy;
 public class ConvertTemperature {
 
     public double[] convertTemperature(double celsius) {
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.8 + 32;
-
-        return new double[] {kelvin, fahrenheit};
+        return new double[] {celsius + 273.15, celsius * 1.8 + 32};
     }
 
 }
