6c225a47ca1a5f30e9d84972d78a359341a63b56
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-01-04 14:31:25 +0300

    add "Divisible and Non-divisible Sums Difference"

---
+++ easy-level/src/main/java/com/sinuke/easy/DivisibleAndNonDivisibleSumsDifference.java
@@ -0,0 +1,16 @@
+package com.sinuke.easy;
+
+public class DivisibleAndNonDivisibleSumsDifference {
+
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) num2 += i;
+            else num1 += i;
+        }
+
+        return num1 - num2;
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/easy/DivisibleAndNonDivisibleSumsDifferenceTest.java
@@ -0,0 +1,28 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DivisibleAndNonDivisibleSumsDifferenceTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void differenceOfSums(int n, int m, int expected) {
+        var diff = new DivisibleAndNonDivisibleSumsDifference();
+        assertEquals(expected, diff.differenceOfSums(n, m));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(10, 3, 19),
+                Arguments.of(5, 6, 15),
+                Arguments.of(5, 1, -15)
+        );
+    }
+
+}
