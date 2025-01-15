33d2b07a3eefa9dad56a42c18e1b464e81d394d6
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-01-15 22:06:51 +0300

    refactor "Running Sum of 1d Array"

--- easy-level/src/main/java/com/sinuke/easy/RunningSumOf1dArray.java
+++ easy-level/src/main/java/com/sinuke/easy/RunningSumOf1dArray.java
@@ -3,14 +3,11 @@ package com.sinuke.easy;
 public class RunningSumOf1dArray {
 
     public int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
-
-        result[0] = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            result[i] = result[i - 1] + nums[i];
+            nums[i] = nums[i - 1] + nums[i];
         }
 
-        return result;
+        return nums;
     }
 
 }
