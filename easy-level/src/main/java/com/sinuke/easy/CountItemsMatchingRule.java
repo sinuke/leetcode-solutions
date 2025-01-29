9fd54e41b8f8b611ce7e1a211e46e126b1c03786
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-01-29 22:24:01 +0300

    refactor "Count Items Matching a Rule"

--- easy-level/src/main/java/com/sinuke/easy/CountItemsMatchingRule.java
+++ easy-level/src/main/java/com/sinuke/easy/CountItemsMatchingRule.java
@@ -6,13 +6,11 @@ import java.util.Map;
 public class CountItemsMatchingRule {
 
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        Map<String, Integer> indx = Map.of(
+        int index = Map.of(
                 "type", 0,
                 "color", 1,
                 "name", 2
-        );
-
-        int index = indx.getOrDefault(ruleKey, 0);
+        ).getOrDefault(ruleKey, 0);
         int count = 0;
 
         for (List<String> item : items) {
