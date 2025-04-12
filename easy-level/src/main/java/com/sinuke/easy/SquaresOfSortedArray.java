55f25c49926afb8488af21ae7c9f8f272e12a0a7
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-04-12 07:30:52 +0300

    fixes

--- easy-level/src/main/java/com/sinuke/easy/SquaresOfSortedArray.java
+++ easy-level/src/main/java/com/sinuke/easy/SquaresOfSortedArray.java
@@ -7,16 +7,24 @@ public class SquaresOfSortedArray {
         int i = 0, j = -1, indx = 0;
 
         while (i < nums.length) {
-            if (nums[i] < 0) j++;
-            else if (nums[i] == 0) result[indx++] = nums[i];
+            if (nums[i] < 0) {
+                j++; i++;
+            }
+            else if (nums[i] == 0) result[indx++] = nums[i++];
             else {
                 if (j >= 0 && nums[i] >= Math.abs(nums[j])) {
                     result[indx++] = nums[j] * nums[j];
-                    result[indx++] = nums[i] * nums[i];
                     j--;
-                } else result[indx++] = nums[i] * nums[i];
+                } else {
+                    result[indx++] = nums[i] * nums[i];
+                    i++;
+                }
             }
-            i++;
+        }
+
+        while (i == nums.length && j >= 0) {
+            result[indx++] = nums[j] * nums[j];
+            j--;
         }
 
         return result;
--- easy-level/src/test/java/com/sinuke/easy/SquaresOfSortedArrayTest.java
+++ easy-level/src/test/java/com/sinuke/easy/SquaresOfSortedArrayTest.java
@@ -20,7 +20,9 @@ class SquaresOfSortedArrayTest {
     private static Stream<Arguments> testData() {
         return Stream.of(
                 Arguments.of(new int[] {-4,-1,0,3,10}, new int[] {0,1,9,16,100}),
-                Arguments.of(new int[] {-7,-3,2,3,11}, new int[] {4,9,9,49,121})
+                Arguments.of(new int[] {-7,-3,2,3,11}, new int[] {4,9,9,49,121}),
+                Arguments.of(new int[] {-1}, new int[] {1}),
+                Arguments.of(new int[] {-10000,-9999,-7,-5,0,0,10000}, new int[] {0,0,25,49,99980001,100000000,100000000})
         );
     }
 
