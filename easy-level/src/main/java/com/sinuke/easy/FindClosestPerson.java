cb32be51ad4ce2f820350f774b01db6bfd1ec4e2
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-04-21 06:52:00 +0300

    refactor "Find Closest Person"

--- easy-level/src/main/java/com/sinuke/easy/FindClosestPerson.java
+++ easy-level/src/main/java/com/sinuke/easy/FindClosestPerson.java
@@ -3,9 +3,9 @@ package com.sinuke.easy;
 public class FindClosestPerson {
 
     public int findClosest(int x, int y, int z) {
-        int result = 0;
-        if (Math.abs(z - x) > Math.abs(z - y)) result = 2;
-        else if (Math.abs(z - y) > Math.abs(z - x)) result = 1;
+        int result = 0, zx = Math.abs(z - x), zy = Math.abs(z - y);
+        if (zx > zy) result = 2;
+        else if (zy > zx) result = 1;
         return result;
     }
 
