39455d1194818a3dcbc30f4123990707e570c840
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-04-28 07:09:10 +0300

    refactor "N-Repeated Element in Size 2N Array"

--- easy-level/src/main/java/com/sinuke/easy/NRepeatedElementInSize2NArray.java
+++ easy-level/src/main/java/com/sinuke/easy/NRepeatedElementInSize2NArray.java
@@ -8,8 +8,8 @@ public class NRepeatedElementInSize2NArray {
     public int repeatedNTimes(int[] nums) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-            if (map.get(num) == nums.length / 2) return num;
+            if (map.containsKey(num)) return num;
+            else map.put(num, 1);
         }
         return 0;
     }
