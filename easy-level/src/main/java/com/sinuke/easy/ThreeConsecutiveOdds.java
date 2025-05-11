02f60ddbe657606b40e41d5076e77cbca98fd4a4
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-05-11 17:01:50 +0300

    refactor "Three Consecutive Odds"

--- easy-level/src/main/java/com/sinuke/easy/ThreeConsecutiveOdds.java
+++ easy-level/src/main/java/com/sinuke/easy/ThreeConsecutiveOdds.java
@@ -3,9 +3,17 @@ package com.sinuke.easy;
 public class ThreeConsecutiveOdds {
 
     public boolean threeConsecutiveOdds(int[] arr) {
-        for (int i = 0; i < arr.length - 2; i++) {
-            if (arr[i] % 2 != 0 && arr[i + 1] %2 != 0 && arr[i + 2] % 2 != 0) return true;
+        int i = 0;
+
+        while (i < arr.length - 2) {
+            if (arr[i] % 2 != 0) {
+                if (arr[i + 1] % 2 != 0) {
+                    if (arr[i + 2] % 2 != 0) return true;
+                    else i += 3;
+                } else i += 2;
+            } else i++;
         }
+
         return false;
     }
 
