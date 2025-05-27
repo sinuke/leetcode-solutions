18019747d8ad64caa1f8ebd4067cc51afa3fd436
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-05-27 06:55:50 +0300

    refactor "Check Distances Between Same Letters"

--- easy-level/src/main/java/com/sinuke/easy/CheckDistancesBetweenSameLetters.java
+++ easy-level/src/main/java/com/sinuke/easy/CheckDistancesBetweenSameLetters.java
@@ -1,19 +1,16 @@
 package com.sinuke.easy;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class CheckDistancesBetweenSameLetters {
 
     public boolean checkDistances(String s, int[] distance) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int[] dp = new int[26];
         for (int i = 0; i < s.length(); i++) {
             int num = s.charAt(i) - 'a';
-            if (map.containsKey(num)) {
-                map.replace(num, i - map.get(num) - 1);
-                if (map.get(num) != distance[num]) return false;
+            if (dp[num] == 0) dp[num] = i == 0 ? -1 : i;
+            else {
+                dp[num] = i - (dp[num] == -1 ? 0 : dp[num]) - 1;
+                if (dp[num] != distance[num]) return false;
             }
-            else map.put(num, i);
         }
         return true;
     }
