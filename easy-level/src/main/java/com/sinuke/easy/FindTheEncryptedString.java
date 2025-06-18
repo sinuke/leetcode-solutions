10ac010aaf0a32f1bd20587f92a3015cd963c497
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-06-18 22:52:51 +0300

    add "Find the Encrypted String"

---
+++ easy-level/src/main/java/com/sinuke/easy/FindTheEncryptedString.java
@@ -0,0 +1,19 @@
+package com.sinuke.easy;
+
+public class FindTheEncryptedString {
+
+    public String getEncryptedString(String s, int k) {
+        char[] result = new char[s.length()];
+        for (int i = 0; i < s.length(); i++) {
+            result[i] = s.charAt(i + k > s.length() - 1 ? i + k - s.length() : i + k);
+        }
+        return new String(result);
+
+//        var result = new StringBuilder();
+//        for (int i = 0; i < s.length(); i++) {
+//            result.append(s.charAt(i + k > s.length() - 1 ? i + k - s.length() : i + k));
+//        }
+//        return result.toString();
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/easy/FindTheEncryptedStringTest.java
@@ -0,0 +1,27 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FindTheEncryptedStringTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void getEncryptedString(String s, int k, String expected) {
+        var solution = new FindTheEncryptedString();
+        assertEquals(expected, solution.getEncryptedString(s, k));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("dart", 3, "tdar"),
+                Arguments.of("aaa", 1, "aaa")
+        );
+    }
+
+}
