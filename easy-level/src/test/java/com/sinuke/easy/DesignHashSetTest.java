32b21acaa2467dfeb59a4feaf7c7618f93eaea4b
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-06-25 07:07:11 +0300

    add "Design HashSet"

---
+++ easy-level/src/test/java/com/sinuke/easy/DesignHashSetTest.java
@@ -0,0 +1,64 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class DesignHashSetTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void myHashSetTest(String[] operations, int[][] values, Object[] expected) {
+        DesignHashSet.MyHashSet myHashSet = null;
+        Object[] actual = new Object[operations.length];
+
+        for (int i = 0; i < operations.length; i++) {
+            var operation = operations[i];
+
+            switch (operation) {
+
+                case "MyHashSet": {
+                    myHashSet = new DesignHashSet.MyHashSet();
+                    actual[i] = null;
+                    break;
+                }
+
+                case "add": {
+                    myHashSet.add(values[i][0]);
+                    actual[i] = null;
+                    break;
+                }
+
+                case "contains": {
+                    actual[i] = myHashSet.contains(values[i][0]);
+                    break;
+                }
+
+                case "remove": {
+                    myHashSet.remove(values[i][0]);
+                    actual[i] = null;
+                    break;
+                }
+
+                default: throw new IllegalArgumentException("Unknown operation: " + operation);
+            }
+        }
+
+        assertArrayEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(
+                        new String[] {"MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"},
+                        new int[][] {{}, {1}, {2}, {1}, {3}, {2}, {2}, {2}, {2}},
+                        new Object[] {null, null, null, true, false, null, true, null, false}
+                )
+        );
+    }
+
+}
