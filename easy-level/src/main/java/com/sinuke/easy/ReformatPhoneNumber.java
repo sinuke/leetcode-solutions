301246f4a5b6d38a0c13b20b88e4ae733f85b699
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-07-16 21:06:51 +0300

    refactor "Reformat Phone Number"

--- easy-level/src/main/java/com/sinuke/easy/ReformatPhoneNumber.java
+++ easy-level/src/main/java/com/sinuke/easy/ReformatPhoneNumber.java
@@ -3,14 +3,18 @@ package com.sinuke.easy;
 public class ReformatPhoneNumber {
 
     public String reformatNumber(String number) {
-        number = number.replaceAll("[ \\-]", "");
+        var numbers = new StringBuilder();
+        for (char c : number.toCharArray()) {
+            if (Character.isDigit(c)) numbers.append(c);
+        }
+
         var sb = new StringBuilder();
-        int start = 0, end = number.length() <= 4 ? 2 : 3;
-        while (end <= number.length()) {
-            sb.append(number, start, end).append("-");
+        int start = 0, end = numbers.length() <= 4 ? 2 : 3;
+        while (end <= numbers.length()) {
+            sb.append(numbers, start, end).append("-");
             start = end;
-            if (number.length() - start <= 4) {
-                if (start + 3 == number.length()) end = start + 3;
+            if (numbers.length() - start <= 4) {
+                if (start + 3 == numbers.length()) end = start + 3;
                 else end = start + 2;
             } else end = start + 3;
         }
