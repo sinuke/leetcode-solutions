498898659d20d5c09ed4b1b7c79774ed567832af
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-07-28 08:07:11 +0300

    refactor "Existence of a Substring in a String and Its Reverse"

--- easy-level/src/main/java/com/sinuke/easy/ExistenceOfSubstringInStringAndItsReverse.java
+++ easy-level/src/main/java/com/sinuke/easy/ExistenceOfSubstringInStringAndItsReverse.java
@@ -8,13 +8,12 @@ public class ExistenceOfSubstringInStringAndItsReverse {
 
     public boolean isSubstringPresent(String s) {
         Set<List<Character>> set = new HashSet<>();
-        for (int i = 0; i < s.length() - 1; i++) {
-            set.add(List.of(s.charAt(i), s.charAt(i + 1)));
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1) set.add(List.of(s.charAt(i), s.charAt(i + 1)));
+            if (i > 0 && set.contains(List.of(s.charAt(i), s.charAt(i - 1)))) return true;
         }
 
-        for (int i = s.length() - 1; i > 0; i--) {
-            if (set.contains(List.of(s.charAt(i), s.charAt(i - 1)))) return true;
-        }
         return false;
     }
 
