590a62e954fb32cd3bb710a7e4a45dafa963a431
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-03 15:48:52 +0300

    add "Perfect Number"

---
+++ easy-level/src/main/java/com/sinuke/easy/PerfectNumber.java
@@ -0,0 +1,18 @@
+package com.sinuke.easy;
+
+public class PerfectNumber {
+
+    public boolean checkPerfectNumber(int num) {
+        int i = 1, sum = 0;
+        long smallest = Math.round(Math.sqrt(num));
+        while (i <= num / smallest) {
+            if (num % i == 0) {
+                sum += i;
+                if (i > 1 && i < smallest) smallest = i;
+            }
+            i++;
+        }
+        return sum == num;
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/easy/PerfectNumberTest.java
@@ -0,0 +1,27 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PerfectNumberTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void checkPerfectNumber(int num, boolean expected) {
+        var solution = new PerfectNumber();
+        assertEquals(expected, solution.checkPerfectNumber(num));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(28, true),
+                Arguments.of(7, false)
+        );
+    }
+
+}
