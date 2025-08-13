93579f3484128436d91149c014dc241814191b01
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-13 07:37:00 +0300

    fix "Group Anagrams"

--- medium-level/src/test/java/com/sinuke/medium/GroupAnagramsTest.java
+++ medium-level/src/test/java/com/sinuke/medium/GroupAnagramsTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -15,16 +17,31 @@ class GroupAnagramsTest {
     @MethodSource("testData")
     void groupAnagrams(String[] strs, List<List<String>> expected) {
         var solution = new GroupAnagrams();
-        assertEquals(expected.size(), solution.groupAnagrams(strs).size());
-        assertEquals(expected.size(), solution.groupAnagrams2(strs).size());
-        assertEquals(expected.size(), solution.groupAnagrams3(strs).size());
+
+        expected.forEach(l -> l.sort(Comparator.naturalOrder()));
+        expected.sort(Comparator.comparingInt(List::size));
+
+        var actual1 = solution.groupAnagrams(strs);
+        actual1.forEach(l -> l.sort(Comparator.naturalOrder()));
+        actual1.sort(Comparator.comparingInt(List::size));
+        assertEquals(expected, actual1);
+
+        var actual2 = solution.groupAnagrams2(strs);
+        actual2.forEach(l -> l.sort(Comparator.naturalOrder()));
+        actual2.sort(Comparator.comparingInt(List::size));
+        assertEquals(expected, actual2);
+
+        var actual3 = solution.groupAnagrams3(strs);
+        actual3.forEach(l -> l.sort(Comparator.naturalOrder()));
+        actual3.sort(Comparator.comparingInt(List::size));
+        assertEquals(expected, actual3);
     }
 
     private static Stream<Arguments> testData() {
         return Stream.of(
-                Arguments.of(new String[] {"eat","tea","tan","ate","nat","bat"}, List.of(List.of("bat"), List.of("nat","tan"), List.of("ate","eat","tea"))),
-                Arguments.of(new String[] {""}, List.of(List.of(""))),
-                Arguments.of(new String[] {"a"}, List.of(List.of("a")))
+                Arguments.of(new String[] {"eat","tea","tan","ate","nat","bat"}, new ArrayList<>(List.of(new ArrayList<>(List.of("bat")), new ArrayList<>(List.of("nat","tan")), new ArrayList<>(List.of("ate","eat","tea"))))),
+                Arguments.of(new String[] {""}, new ArrayList<>(List.of(new ArrayList(List.of(""))))),
+                Arguments.of(new String[] {"a"}, new ArrayList<>(List.of(new ArrayList(List.of("a")))))
         );
     }
 
