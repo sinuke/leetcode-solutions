fb0c9ecb680c9af384eba11b2b6376a5eef5ab71
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-14 09:18:03 +0300

    fix "Minimum Add to Make Parentheses Valid"

--- medium-level/src/main/java/com/sinuke/medium/MinimumAddToMakeParenthesesValid.java
+++ medium-level/src/main/java/com/sinuke/medium/MinimumAddToMakeParenthesesValid.java
@@ -1,14 +1,16 @@
 package com.sinuke.medium;
 
+import java.util.Stack;
+
 public class MinimumAddToMakeParenthesesValid {
 
     public int minAddToMakeValid(String s) {
-        int count = 0;
+        Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
-            if (c == '(') count++;
-            else count--;
+            if (c == ')' && !stack.isEmpty() && stack.peek() == '(') stack.pop();
+            else stack.push(c);
         }
-        return Math.abs(count);
+        return stack.size();
     }
 
 }
