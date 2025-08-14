ca66af1e6f78bc640526789cc352c78c0d35bbd7
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-14 11:02:14 +0300

    refactor "Largest 3-Same-Digit Number in String"

--- easy-level/src/main/java/com/sinuke/easy/Largest3SameDigitNumberInString.java
+++ easy-level/src/main/java/com/sinuke/easy/Largest3SameDigitNumberInString.java
@@ -2,21 +2,22 @@ package com.sinuke.easy;
 
 public class Largest3SameDigitNumberInString {
 
-    // 2 ms
     public String largestGoodInteger(String num) {
-        int counter = 1;
-        char prev = num.charAt(0);
-        String result = "";
-        for (int i = 1; i < num.length(); i++) {
-            if (prev == num.charAt(i)) {
+        int counter = 0, max = -1, prev = -1;
+        var result = "";
+        for (int i = 0; i < num.length(); i++) {
+            int val = num.charAt(i) - '0';
+            if (prev == val) {
                 counter++;
                 if (counter == 3) {
-                    String str = num.substring(i - 2, i + 1);
-                    if (str.compareTo(result) > 0) result = str;
+                    if (val > max) {
+                        result = num.substring(i - 2, i + 1);
+                        max = val;
+                    }
                     counter = 1;
                 }
             } else counter = 1;
-            prev = num.charAt(i);
+            prev = val;
         }
         return result;
     }
