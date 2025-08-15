9a426026015ad01b293e68405fad008cb221ccd9
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-15 23:00:32 +0300

    add "Partition Labels"

---
+++ medium-level/src/main/java/com/sinuke/medium/PartitionLabels.java
@@ -0,0 +1,35 @@
+package com.sinuke.medium;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PartitionLabels {
+
+    public List<Integer> partitionLabels(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), i);
+        }
+
+        int i = 0;
+        List<Integer> result = new ArrayList<>();
+        while (i < s.length()) {
+            int pos = map.get(s.charAt(i));
+
+            int j = pos;
+            while (j > i) {
+                if (map.get(s.charAt(j)) > pos) pos = map.get(s.charAt(j));
+                j--;
+            }
+
+            int len = pos - i + 1;
+            result.add(len);
+            i = pos + 1;
+        }
+
+        return result;
+    }
+
+}
---
+++ medium-level/src/test/java/com/sinuke/medium/PartitionLabelsTest.java
@@ -0,0 +1,29 @@
+package com.sinuke.medium;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PartitionLabelsTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void partitionLabels(String s, List<Integer> expected) {
+        var solution = new PartitionLabels();
+        assertEquals(expected, solution.partitionLabels(s));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("ababcbacadefegdehijhklij", List.of(9, 7, 8)),
+                Arguments.of("eccbbbbdec", List.of(10)),
+                Arguments.of("e", List.of(1))
+        );
+    }
+
+}
