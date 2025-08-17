decb3d6bbf077be7aa7f2cc0885331b85e603f39
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-17 14:34:56 +0300

    add "Merge Intervals"

---
+++ medium-level/src/main/java/com/sinuke/medium/MergeIntervals.java
@@ -0,0 +1,21 @@
+package com.sinuke.medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MergeIntervals {
+
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        int i = 1, k = 0;
+        while (i < intervals.length) {
+            if (intervals[i][0] <= intervals[k][1]) intervals[k][1] = intervals[i][1];
+            else intervals[++k] = intervals[i];
+            i++;
+        }
+
+        return Arrays.copyOf(intervals, k + 1);
+    }
+
+}
---
+++ medium-level/src/test/java/com/sinuke/medium/MergeIntervalsTest.java
@@ -0,0 +1,27 @@
+package com.sinuke.medium;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class MergeIntervalsTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void merge(int[][] intervals, int[][] expected) {
+        var solution = new MergeIntervals();
+        assertArrayEquals(expected, solution.merge(intervals));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(new int[][] {{1,3},{2,6},{8,10},{15,18}}, new int[][] {{1,6},{8,10},{15,18}}),
+                Arguments.of(new int[][] {{1,4},{4,5}}, new int[][] {{1,5}})
+        );
+    }
+
+}
