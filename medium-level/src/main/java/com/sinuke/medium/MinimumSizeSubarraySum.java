b4e99af6d1a8452c9e6d45a3744b8525f5c1f070
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-22 15:47:29 +0300

    fix "Minimum Size Subarray Sum"

--- medium-level/src/main/java/com/sinuke/medium/MinimumSizeSubarraySum.java
+++ medium-level/src/main/java/com/sinuke/medium/MinimumSizeSubarraySum.java
@@ -1,22 +1,16 @@
 package com.sinuke.medium;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MinimumSizeSubarraySum {
 
     public int minSubArrayLen(int target, int[] nums) {
-        int min = Integer.MAX_VALUE, sum = 0;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
+        int min = Integer.MAX_VALUE, sum = 0, start = 0;
 
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (nums[i] == target) return 1;
-            else if (sum == target || map.containsKey(target - nums[i])) min = Math.min(min, i - map.get(target - nums[i]));
-
-            map.put(sum, i);
+            while (sum >= target) {
+                min = Math.min(min, i - start + 1);
+                sum -= nums[start++];
+            }
         }
 
         return min == Integer.MAX_VALUE ? 0 : min;
--- medium-level/src/test/java/com/sinuke/medium/MinimumSizeSubarraySumTest.java
+++ medium-level/src/test/java/com/sinuke/medium/MinimumSizeSubarraySumTest.java
@@ -21,7 +21,11 @@ class MinimumSizeSubarraySumTest {
         return Stream.of(
                 Arguments.of(7, new int[] {2,3,1,2,4,3}, 2),
                 Arguments.of(4, new int[] {1,4,4}, 1),
-                Arguments.of(11, new int[] {1,1,1,1,1,1,1,1}, 0)
+                Arguments.of(11, new int[] {1,1,1,1,1,1,1,1}, 0),
+                Arguments.of(11, new int[] {1,2,3,4,5}, 3),
+                Arguments.of(15, new int[] {1,2,3,4,5}, 5),
+                Arguments.of(15, new int[] {5,1,3,5,10,7,4,9,2,8}, 2),
+                Arguments.of(80, new int[] {10,5,13,4,8,4,5,11,14,9,16,10,20,8}, 6)
         );
     }
 
