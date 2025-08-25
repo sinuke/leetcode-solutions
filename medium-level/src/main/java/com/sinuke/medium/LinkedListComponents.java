d4bf0e3a9bcba89da138f125816b57a1d5908d06
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-08-25 19:52:17 +0300

    fix

--- medium-level/src/main/java/com/sinuke/medium/LinkedListComponents.java
+++ medium-level/src/main/java/com/sinuke/medium/LinkedListComponents.java
@@ -8,24 +8,6 @@ import java.util.Set;
 public class LinkedListComponents {
 
     public int numComponents(ListNode head, int[] nums) {
-        /*
-        int[] arr = new int[nums.length + 1];
-        for (int num : nums) arr[num]++;
-
-        int result = 0, count = 0;
-        while (head != null) {
-            if (arr[head.val] > 0) count++;
-            else {
-                if (count != 0) result++;
-                count = 0;
-            }
-
-            head = head.next;
-        }
-
-        return count == 0 ? result : ++result;
-         */
-
         Set<Integer> set = new HashSet<>(nums.length);
         for (int num : nums) set.add(num);
 
