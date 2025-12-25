64ce97b98d67fa67972bbb93867f8a6cd8d7bfdb
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2025-12-25 20:17:06 +0300

    add "Minimum Number of Pushes to Type Word I"

---
+++ easy-level/src/test/java/com/sinuke/easy/MinimumNumberOfPushesToTypeWordITest.java
@@ -0,0 +1,27 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MinimumNumberOfPushesToTypeWordITest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void minimumPushes(String word, int expected) {
+        var solution = new MinimumNumberOfPushesToTypeWordI();
+        assertEquals(expected, solution.minimumPushes(word));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("abcde", 5),
+                Arguments.of("xycdefghij", 12)
+        );
+    }
+
+}
