90248e4bdcfe6e811dcec1cba274f47bbd4ab686
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-01-03 07:16:17 +0300

    added "Interval Cancellation"

---
+++ easy-level/js/2725. Interval Cancellation/IntervalCancellation.js
@@ -0,0 +1,53 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+var cancellable = function(fn, args, t) {
+    const results = [];
+
+    const firstResult = fn(...args);
+    results.push({ time: 0, returned: firstResult });
+
+    const intervalId = setInterval(() => {
+        const result = fn(...args);
+        const currentTime = Date.now() - startTime;
+        results.push({ time: currentTime, returned: result });
+    }, t);
+
+    const startTime = Date.now();
+
+    return () => {
+        clearInterval(intervalId);
+    };
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 2;
+ *  const args = [4], t = 35, cancelTimeMs = 190;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [
+ *                           //     {"time":0,"returned":8},
+ *                           //     {"time":35,"returned":8},
+ *                           //     {"time":70,"returned":8},
+ *                           //     {"time":105,"returned":8},
+ *                           //     {"time":140,"returned":8},
+ *                           //     {"time":175,"returned":8}
+ *                           // ]
+ *  }, cancelTimeMs + t + 15)
+ */
