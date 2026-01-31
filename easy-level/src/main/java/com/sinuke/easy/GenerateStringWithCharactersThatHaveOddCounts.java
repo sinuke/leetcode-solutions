6fd6f396bad405fb6f46f70eddac7c4aeaee0607
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-01-31 06:35:01 +0300

    fix "Generate a String With Characters That Have Odd Counts"

--- easy-level/src/main/java/com/sinuke/easy/GenerateStringWithCharactersThatHaveOddCounts.java
+++ easy-level/src/main/java/com/sinuke/easy/GenerateStringWithCharactersThatHaveOddCounts.java
@@ -3,7 +3,7 @@ package com.sinuke.easy;
 public class GenerateStringWithCharactersThatHaveOddCounts {
 
     public String generateTheString(int n) {
-        return "a".repeat(Math.max(0, n - 1)) + (n % 2 == 0 ? 'b' : 'a');
+        return "a".repeat(n - 1) + (n % 2 == 0 ? 'b' : 'a');
     }
 
 }
