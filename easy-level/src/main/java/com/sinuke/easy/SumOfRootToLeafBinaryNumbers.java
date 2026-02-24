067597bfc2f7f49cc6136ac52fbe4a08ea439588
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-02-24 07:22:36 +0300

    update "Sum of Root To Leaf Binary Numbers"

--- easy-level/src/main/java/com/sinuke/easy/SumOfRootToLeafBinaryNumbers.java
+++ easy-level/src/main/java/com/sinuke/easy/SumOfRootToLeafBinaryNumbers.java
@@ -2,29 +2,30 @@ package com.sinuke.easy;
 
 import com.sinuke.common.data.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class SumOfRootToLeafBinaryNumbers {
 
     public int sumRootToLeaf(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        extractValue(root, new StringBuilder(), list);
-        return list.stream().mapToInt(Integer::intValue).sum();
+        Value value = new Value();
+        extractValue(root, new StringBuilder(), value);
+        return value.sum;
     }
 
-    private void extractValue(TreeNode node, StringBuilder sb, List<Integer> list) {
+    private void extractValue(TreeNode node, StringBuilder sb, Value value) {
         if (node == null) return;
 
         sb.append(node.val);
 
         if (node.left == null && node.right == null) {
-            list.add(Integer.parseInt(sb.toString(), 2));
+            value.sum += Integer.parseInt(sb.toString(), 2);
             return;
         }
 
-        extractValue(node.left, new StringBuilder(sb), list);
-        extractValue(node.right, new StringBuilder(sb), list);
+        extractValue(node.left, new StringBuilder(sb), value);
+        extractValue(node.right, new StringBuilder(sb), value);
+    }
+
+    private static class Value {
+        int sum;
     }
 
 }
