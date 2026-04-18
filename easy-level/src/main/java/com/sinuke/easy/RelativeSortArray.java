23550334d50ffc095d3667c711bbb6a5ed1ca027
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-04-18 07:25:50 +0300

    fix "Relative Sort Array"

--- easy-level/src/main/java/com/sinuke/easy/RelativeSortArray.java
+++ easy-level/src/main/java/com/sinuke/easy/RelativeSortArray.java
@@ -9,13 +9,16 @@ public class RelativeSortArray {
 
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
         for (int i = 0; i < arr2.length; i++) {
             map.put(arr2[i], i);
+            max = Math.max(max, arr2[i]);
         }
+        final int maxFinal = max;
 
         return Arrays.stream(arr1)
                 .boxed()
-                .sorted(Comparator.comparingInt(i -> map.getOrDefault(i, i)))
+                .sorted(Comparator.comparingInt(i -> map.getOrDefault(i, i + maxFinal)))
                 .mapToInt(Integer::intValue)
                 .toArray();
     }
