b9dad19da93091e6a7695b918958bae4a103e155
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-05-10 23:25:55 +0300

    additional solution for "Move Zeroes"

--- easy-level/src/main/java/com/sinuke/easy/MoveZeroes.java
+++ easy-level/src/main/java/com/sinuke/easy/MoveZeroes.java
@@ -2,7 +2,23 @@ package com.sinuke.easy;
 
 public class MoveZeroes {
 
+    // 2 ms
     public void moveZeroes(int[] nums) {
+        int s = 0, f = 0;
+        while (f < nums.length) {
+            if (nums[f] != 0) {
+                int tmp = nums[s];
+                nums[s] = nums[f];
+                nums[f] = tmp;
+
+                s++;
+            }
+            f++;
+        }
+    }
+
+    // 1 ms
+    public void moveZeroes2(int[] nums) {
         int[] temp = new int[nums.length];
         int j = -1;
         for (int num : nums) {
