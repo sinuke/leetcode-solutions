1f1e9cea1a39dfc95bf8e01ef8807f953ca55441
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-05-11 06:59:24 +0300

    update for "Separate the Digits in an Array"

--- easy-level/src/main/java/com/sinuke/easy/SeparateTheDigitsInArray.java
+++ easy-level/src/main/java/com/sinuke/easy/SeparateTheDigitsInArray.java
@@ -1,18 +1,20 @@
 package com.sinuke.easy;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SeparateTheDigitsInArray {
 
     public int[] separateDigits(int[] nums) {
-        List<Integer> lst = new LinkedList<>();
+        List<Integer> lst = new ArrayList<>(nums.length * 6);
+
         for (int num : nums) {
             String numStr = String.valueOf(num);
             for (char c : numStr.toCharArray()) {
                 lst.add(c - '0');
             }
         }
+
         return lst.stream().mapToInt(i -> i).toArray();
     }
 
