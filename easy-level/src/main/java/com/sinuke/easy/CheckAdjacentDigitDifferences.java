0aa02e2d520c8501a605f67de83b17202dd80ac4
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-05-17 20:45:52 +0300

    add "Check Adjacent Digit Differences"

---
+++ easy-level/src/main/java/com/sinuke/easy/CheckAdjacentDigitDifferences.java
@@ -0,0 +1,12 @@
+package com.sinuke.easy;
+
+public class CheckAdjacentDigitDifferences {
+
+    public boolean isAdjacentDiffAtMostTwo(String s) {
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (Math.abs(s.charAt(i) - s.charAt(i + 1)) > 2) return false;
+        }
+        return true;
+    }
+
+}
---
+++ easy-level/src/test/java/com/sinuke/easy/CheckAdjacentDigitDifferencesTest.java
@@ -0,0 +1,26 @@
+package com.sinuke.easy;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CheckAdjacentDigitDifferencesTest {
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void isAdjacentDiffAtMostTwo(String s, boolean expected) {
+        assertEquals(expected, new CheckAdjacentDigitDifferences().isAdjacentDiffAtMostTwo(s));
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("132", true),
+                Arguments.of("129", false)
+        );
+    }
+
+}
