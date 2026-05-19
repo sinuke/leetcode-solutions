8aca297e4c126467779ae88bdb3df0298315ce83
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-05-19 07:22:58 +0300

    add "K-th Smallest Prime Fraction"

--- medium-level/src/main/java/com/sinuke/medium/KthSmallestPrimeFraction.java
+++ medium-level/src/main/java/com/sinuke/medium/KthSmallestPrimeFraction.java
@@ -7,7 +7,7 @@ public class KthSmallestPrimeFraction {
 
     // 682 ms
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.<int[]>comparingDouble(a -> ((double) a[0])/a[1]));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingDouble(a -> ((double) a[0])/a[1]));
         for (int i = 0; i < arr.length; i++) {
             for (int j = arr.length - 1; j > i; j--) {
                 pq.add(new int[] {arr[i], arr[j]});
