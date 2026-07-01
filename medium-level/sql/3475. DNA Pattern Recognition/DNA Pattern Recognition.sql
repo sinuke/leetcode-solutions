f80ad52cccb661a101ab2bea4bc6980784be8918
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-07-01 22:31:33 +0300

    fix "DNA Pattern Recognition"

--- medium-level/sql/3475. DNA Pattern Recognition/DNA Pattern Recognition.sql
+++ medium-level/sql/3475. DNA Pattern Recognition/DNA Pattern Recognition.sql
@@ -6,5 +6,5 @@ SELECT
     dna_sequence LIKE '%TAG' OR dna_sequence LIKE '%TAA' OR dna_sequence LIKE '%TGA' AS has_stop,
     dna_sequence LIKE '%ATAT%' AS has_atat,
     dna_sequence LIKE '%GGG%' AS has_ggg
-FROM samples
+FROM Samples
 ORDER BY sample_id ASC;
