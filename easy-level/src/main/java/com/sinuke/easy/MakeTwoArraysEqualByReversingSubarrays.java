d5f57835da79c101ed2e112b60287eec61e43273
Stanislau Ihnatovich <stanislau_ihnatovich@epam.com>
2026-07-11 07:35:16 +0300

    fix "Make Two Arrays Equal by Reversing Subarrays"

--- easy-level/src/main/java/com/sinuke/easy/MakeTwoArraysEqualByReversingSubarrays.java
+++ easy-level/src/main/java/com/sinuke/easy/MakeTwoArraysEqualByReversingSubarrays.java
@@ -11,10 +11,10 @@ public class MakeTwoArraysEqualByReversingSubarrays {
         for (int i = 0; i < target.length; i++) {
             if (target[i] != arr[i]) cnt++;
             map.put(target[i], map.getOrDefault(target[i], 0) + 1);
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+            map.put(arr[i], map.getOrDefault(arr[i], 0) - 1);
         }
 
-        return cnt == 0 || map.entrySet().stream().noneMatch(e -> e.getValue() % 2 != 0);
+        return cnt == 0 || map.entrySet().stream().noneMatch(e -> e.getValue() != 0);
     }
 
 }
